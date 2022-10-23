e67935f928e36d594d9a4df9b1b875696f4b647b
Francis Maina <sebastianfrancix@gmail.com>
2022-10-23 05:55:26 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    # name getter
+    def name=(name)
+        @name = name
+    end
 
+    # name setter 
+    def name
+        @name
+    end
+
+    # breed getter 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # breed setter 
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
+    # name getter
+    def name=(name)
+        @name = name
+    end
 
+    # name setter 
+    def name
+        @name
+    end
+
+    # job getter 
+    def job=(job)
+        @job = job
+    end
+
+    # job setter 
+    def job
+        @job
+    end
 end
